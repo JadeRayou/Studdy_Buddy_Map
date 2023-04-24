7021a337b3a339f2ac35c23fb79a4198cd6430a2
sidibe-bintou <sidib.bintou@gmail.com>
2023-04-24 14:46:08 +0200

    modif bouton

--- main.js
+++ main.js
@@ -4,3 +4,26 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+// Ajout des markers depuis notre API interne
+// Fonction qui sert à ajouter un marker dans la db
+// Une fois que le marker est ajouté, on actualise la map
+function addMarker(title, description, coord) {
+    fetch('/marker', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            title: title,
+            description: description,
+            coord: coord
+        })
+    })
+    .then(function () {
+        updateMarkers();
+    })
+    .catch(function (err) {
+        console.error(err);
+    })
+}
